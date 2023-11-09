36ba72dd1d534ccea827d0a3b71106003645c3e7
Jayden12234 <130702014+Jayden12234@users.noreply.github.com>
2023-11-09 09:10:35 +0000

    new feture

--- map_class.py
+++ map_class.py
@@ -2,8 +2,9 @@
 import random
 
 class Map:
-    def __init__(self, conv_key, har_key, slm_key, mat_key):
+    def __init__(self, conv_key, har_key, slm_key, mat_key, out_f):
         self.size = [15,15]
+        self.out_f = out_f
         self.mat_key = mat_key
         self.map = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -28,3 +29,5 @@ class Map:
             for j in range(self.size[1]):
                 if random.randint(0,10)==2:
                     self.map[j][i] = self.mat_key
+    def write_val(self,pos,val):
+        self.map[pos[1]][pos[0]] = val
