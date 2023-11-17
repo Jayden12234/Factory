85eeafda7e5308195d8b20184ff21dd93296c012
Jayden12234 <130702014+Jayden12234@users.noreply.github.com>
2023-11-17 11:15:57 +0000

    new

--- con_class.py
+++ con_class.py
@@ -1,2 +1,12 @@
 
-class Co
+class Coneyer:
+    def __init__(self, pos, dic):
+        self.pos = pos
+        self.dic = dic
+        self.obj = None
+    def add_conv(self, conv):
+        if conv.pos==[self.pos[0]-self.dic[0], self.pos[1]-self.dic[1]]:
+            self.conv = conv
+    def send_obj(self):
+        if self.obj != None:
+            self.conv.obj = self.obj
